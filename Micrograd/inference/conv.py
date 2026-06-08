e0fda91ac858728f40f34a128fa319f5380004ff
msck72 <chaitanya.mangipudi2002@gmail.com>
2026-06-08 22:29:10 +0530

    Add support for Conv 3D for processing 3 RGB images

--- Micrograd/inference/conv.py
+++ Micrograd/inference/conv.py
@@ -52,3 +52,52 @@ class Conv2D:
                     output_tensor[i, j] = temp
 
         return output_tensor
+
+class Conv3D:
+    def __init__(self, filter_size, depth, value=None):
+        self.filter_size = filter_size
+        self.depth = depth
+        if value is not None:
+            self.filter = InferenceTensor((depth, filter_size, filter_size), value)
+        else:
+            self.filter = InferenceTensor((depth, filter_size, filter_size))
+        
+    def set_filter(self, filter: np.ndarray):
+        assert filter.shape == (self.depth, self.filter_size, self.filter_size), "Filter size mismatch"
+        self.filter.set_values(filter)
+    
+    def __call__(self, input_tensor, **kwargs):
+        assert len(input_tensor.shape) == 3, "The input tensor dimension must be 3 while applying conv 3D"
+        input_depth, input_x, input_y = input_tensor.shape
+        assert input_depth == self.depth, "Input depth must match filter depth"
+
+        output_tensor = InferenceTensor((input_x - self.filter_size + 1, input_y - self.filter_size + 1))
+
+        if ("use_threads" in kwargs) and kwargs["use_threads"]:
+
+            def _convolve(i, j):
+                temp = 0
+                for d in range(self.depth):
+                    for m in range(self.filter_size):
+                        for n in range(self.filter_size):
+                            temp += self.filter[d, m, n] * input_tensor[d, i + m, j + n]
+                output_tensor[i, j] = temp
+
+
+            with ThreadPoolExecutor(max_workers=4) as executor:
+                futures = []
+                for i in range(input_x - self.filter_size + 1):
+                    for j in range(input_y - self.filter_size + 1):
+                        futures.append(executor.submit(_convolve, i, j))
+        else:
+            for i in range(input_x - self.filter_size + 1):
+                for j in range(input_y - self.filter_size + 1):
+                    temp = 0
+                    for d in range(self.depth):
+                        for m in range(self.filter_size):
+                            for n in range(self.filter_size):
+                                temp += self.filter[d, m, n] * input_tensor[d, i + m, j + n]
+                    output_tensor[i, j] = temp
+
+        return output_tensor
+
